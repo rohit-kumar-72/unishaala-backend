a2ef1cb35b73e7ea9f64732424ed26e6c0949643
rohit-kumar-72 <rohitraj72342@gmail.com>
2025-02-18 10:56:24 +0530

    added frontend cors setting

--- unishaala/settings.py
+++ unishaala/settings.py
@@ -205,5 +205,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost","api.unishaala.com"]
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://unishaala.com",
+    "https://www.unishaala.com"
 ]
 
